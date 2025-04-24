36c4dda9ea15303d1819dfa92eead876afc8b6e6
renegeda <54241294+renegeda@users.noreply.github.com>
2025-04-24 22:22:48 +0500

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,7 +1,7 @@
 <?php
 define('DB_HOST', 'localhost');
-define('DB_USER', 'j20548365_manger');
-define('DB_PASS', 'gYz)frmkdv5s');
-define('DB_NAME', 'j20548365_ordermanager');
-define('BASE_URL', 'http://86c9939bcf3f.hosting.myjino.ru/tso/');
+define('DB_USER', 'user');
+define('DB_PASS', 'password');
+define('DB_NAME', 'db_name');
+define('BASE_URL', 'site url');
 ?>
